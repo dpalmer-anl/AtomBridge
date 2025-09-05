99da7f4d5685ad204780c53376fec44ce7bb6d37
dpalmer-anl <dpalmer3@illinois.edu>
2025-09-05 11:13:29 -0500

    add a langchain example

--- example.py
+++ example.py
@@ -1,3 +1,4 @@
+# Import relevant functionality
 from langchain.chat_models import init_chat_model
 from langchain_tavily import TavilySearch
 from langgraph.checkpoint.memory import MemorySaver
@@ -18,3 +19,33 @@ search = TavilySearch(max_results=2)
 tools = [search, multiply]
 agent_executor = create_react_agent(model, tools, checkpointer=memory)
 config = {"configurable": {"thread_id": "abc123"}}
+
+
+input_message = {
+    "role": "user",
+    "content": "Hi, I'm Dan and I live in Champaign.",
+}
+for step in agent_executor.stream(
+    {"messages": [input_message]}, config, stream_mode="values"
+):
+    step["messages"][-1].pretty_print()
+
+input_message = {
+    "role": "user",
+    "content": "What's the weather where in Champaign?",
+}
+
+for step in agent_executor.stream(
+    {"messages": [input_message]}, config, stream_mode="values"
+):
+    step["messages"][-1].pretty_print()
+
+input_message = {
+    "role": "user",
+    "content": "What is 3.4 * 8.5?",
+}
+
+for step in agent_executor.stream(
+    {"messages": [input_message]}, config, stream_mode="values"
+):
+    step["messages"][-1].pretty_print()
